28dcad08ac42ab183fd14f7a6ed5b86f97a24ce0
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-27 20:16:55 -0500

    added logic to accept content to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,15 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//Added logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.error('Data has been saved to database', result);
+}
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
