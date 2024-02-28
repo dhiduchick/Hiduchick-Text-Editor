5fab9bf7fc7d0306cea3ce63ae9caa32e673cc8b
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-27 21:33:09 -0500

    Added and configured workbox plugins for a service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -13,17 +13,59 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    devtool: 'source-map',
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Text Editor'
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text Editor',
+        short_name: 'Editor',
+        description: 'Perfect way to never forget your notes',
+        background_color: '#B2AC88',
+        theme_color: '#F4C2C2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96,128,192,256,384,512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'bable-loader',
+            options: {
+              presets: ['@bable/preset-env'],
+              plugins: ['@bable/plugin-proposal-object-rest-spread', '@bable/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
