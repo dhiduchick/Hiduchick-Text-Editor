7960a930e77f598bdfbc946fd28e17a16be485d9
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-27 20:43:09 -0500

    clear prompt

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -21,4 +21,6 @@ butInstall.addEventListener('click', async () => {
 });
 
 
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
