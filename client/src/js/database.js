1a32dab940912944a5a22dc8e6ca5a216a13a166
Daelyn Hiduchick <DHIDUCHICK@GMAIL.COM>
2024-02-28 20:11:23 -0500

    adding files and adding methode to accept content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,24 +12,17 @@ const initdb = async () =>
     },
   });
 
-//Added logic to a method that accepts some content and adds it to the database
+// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const request = store.put({id: 1, value: content});
+  const request = store.put({ id: 1, value: content });
   const result = await request;
-  console.error('Data has been saved to database', result);
+  console.log('Data saved to the database', result);
 }
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => {
-  const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
-  const result = await request;
-  console.error(`request.value`, result);
-  return result?.value;
-}
+export const getDb = async () => console.error('getDb not implemented');
+
 initdb();
